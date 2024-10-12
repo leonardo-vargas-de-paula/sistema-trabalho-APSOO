2895a6ca72fb45bec49267d36420dc2a0b1381d4
jordanna <jordanabruna90@gmil.com>
2024-10-12 00:20:23 -0400

    Adiciona demais classes correspondentes a pasta model geradas a partir do diagrama de classes

---
+++ src/main/java/sistema/model/ClienteConcreto.java
@@ -0,0 +1,23 @@
+public class ClienteConcreto implements ClienteRepository {
+
+	private void salvar() {
+
+	}
+
+	private void obterPeloId() {
+
+	}
+
+	private void deletar() {
+
+	}
+
+
+	/**
+	 * @see ClienteRepository#obterPorId()
+	 */
+	private void obterPorId() {
+
+	}
+
+}
