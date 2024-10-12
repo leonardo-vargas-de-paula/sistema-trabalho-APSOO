780dcb3a87e973b8a458962ed84e769c39b1c3fe
jordanna <jordanabruna90@gmil.com>
2024-10-12 00:12:04 -0400

    Cria pacote repository e  adiciona classes correspondentes a pasta model geradas a partir do diagrama de classes

---
+++ src/main/java/sistema/model/Pagamento.java
@@ -0,0 +1,35 @@
+public abstract class Pagamento {
+
+	private String ID;
+
+	private float valor;
+
+	private String status;
+
+	private Date data;
+
+	public void getId() {
+
+	}
+
+	public void getValor() {
+
+	}
+
+	public void getStatus() {
+
+	}
+
+	public void setId() {
+
+	}
+
+	public void setValor() {
+
+	}
+
+	public void setStatus() {
+
+	}
+
+}
---
+++ src/main/java/sistema/model/Usuario.java
@@ -0,0 +1,27 @@
+public class Usuario {
+
+	private String ID;
+
+	private String senha;
+
+	private void login() {
+
+	}
+
+	public void getId() {
+
+	}
+
+	public void getSenha() {
+
+	}
+
+	public void setId() {
+
+	}
+
+	public void setSenha() {
+
+	}
+
+}
